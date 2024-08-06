9a80258ea1ecdaa928b9c4603e71779756e299dc
Omkar Biradar <141556018+Ombiradar07@users.noreply.github.com>
2024-08-06 15:07:26 +0530

    Update DecimalToBinary.java

--- Day_5/DecimalToBinary.java
+++ Day_5/DecimalToBinary.java
@@ -1,7 +1,20 @@
 package Day_5;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
-	public static void main(String[] args) {
+	
+	static String decimalToBinary(int decimal) {
 		
+		return Integer.toBinaryString(decimal);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Decimal number....");
+		int decimal = sc.nextInt();
+		
+		String binary = decimalToBinary(decimal);
+		
+		System.out.println("Decimal : "+ decimal+" -> Binary : "+binary);
 	}
 }
