f6408f6a8ac06c8dfe6c028cdb8e972286a641fd
Omkar Biradar <141556018+Ombiradar07@users.noreply.github.com>
2024-08-06 15:05:34 +0530

    Update BinaryToDecimal.java

--- Day_5/BinaryToDecimal.java
+++ Day_5/BinaryToDecimal.java
@@ -6,23 +6,24 @@ public class BinaryToDecimal {
 	
 	static int binaryToDecimal(String binary){//1100
 		
-		int decimal = 0 , base=1;
-		 for(int i =binary.length()-1;i>=0;i--) {
-			 
-			 if(binary.charAt(i)=='1')
-				 decimal+=base;
-			 
-			 base*=2;
-		 }
-		 
-		 return decimal;
+		/*
+		 * int decimal = 0 , base=1; for(int i =binary.length()-1;i>=0;i--) {
+		 * 
+		 * if(binary.charAt(i)=='1') decimal+=base;
+		 * 
+		 * base*=2; }
+		 */
+		int decimal = Integer.parseInt(binary, 2);
+				 return decimal;
 	}
 	
 	public static void main(String[] args) {
-		String binary;
+		
 		System.out.println("Enter a Binary number....");
 		Scanner sc = new Scanner(System.in);
-		binary = sc.nextLine();
-		System.out.println(binaryToDecimal(binary));
+		String binary = sc.nextLine();
+		int decimal = binaryToDecimal(binary);
+		
+		System.out.println("Binary : "+binary+" -> Decimal : "+decimal);
 	}
 }
