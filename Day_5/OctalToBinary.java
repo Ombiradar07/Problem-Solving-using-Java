faacfa3f10e0371ead48813b0c7a1d8da2575bbe
Omkar Biradar <141556018+Ombiradar07@users.noreply.github.com>
2024-08-06 15:08:11 +0530

    Update OctalToBinary.java

--- Day_5/OctalToBinary.java
+++ Day_5/OctalToBinary.java
@@ -1,7 +1,21 @@
 package Day_5;
 
-public class OctalToBinary {
-	public static void main(String[] args) {
+import java.util.Scanner;
+
+public class DecimalToOctal {
+	
+	static String  decimalToOctal(int decimal) {
+			
+		return Integer.toOctalString(decimal);
+	}
+public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Decimal number....");
+		int decimal = sc.nextInt();
 		
+		String octal = decimalToOctal(decimal);
+		
+		System.out.println("Decimal : "+ decimal+" -> Octal : "+octal);
 	}
 }
+
